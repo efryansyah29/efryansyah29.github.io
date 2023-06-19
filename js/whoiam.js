66318e239adc220115c5138ca2b4b33dbd1d9a33
efryansyah29 <78467375+efryansyah29@users.noreply.github.com>
2023-06-19 20:22:51 +0700

    Add files via upload

---
+++ js/whoiam.js
@@ -0,0 +1,17 @@
+new Typed('#typed', {
+  strings: [
+    'A.K.A Ryan',
+    'Graduate Student',
+    'IT Specialist',
+    'Freelance IT Support',
+    'Fresh Graduated',
+    'Infinite Learners',
+    'Creative Thinker',
+    'Educator',
+  ],
+  typeSpeed: 80,
+  backSpeed: 40,
+  backDelay: 500,
+  loop: true,
+  smartBackspace: true,
+});
